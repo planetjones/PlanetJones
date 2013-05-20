38e432e47b9a7dd9ef8c098151604516e60ec2c5
Jonathan Jones <jon.r.jones@gmail.com>
2013-05-20 19:20:07 +0200

    Added a dirtty implementation of links to servlet so jobs can be submitted, restarted and viewed

--- test-jsr-352-java-batch/src/main/java/co/uk/planetjones/BatchJobStartServlet.java
+++ test-jsr-352-java-batch/src/main/java/co/uk/planetjones/BatchJobStartServlet.java
@@ -3,6 +3,7 @@ package co.uk.planetjones;
 
 import com.sun.org.apache.bcel.internal.classfile.ExceptionTable;
 
+import javax.batch.operations.JobExecutionAlreadyCompleteException;
 import javax.batch.runtime.JobExecution;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -13,8 +14,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet (name="BatchJobStartServlet", urlPatterns="/run_batch_job")
+@WebServlet(name = "BatchJobStartServlet", urlPatterns = BatchJobStartServlet.SERVLET_MAPPING)
 public class BatchJobStartServlet extends HttpServlet {
+
+    final static String SERVLET_MAPPING = "/run_batch_job";
+    final static String HYPERLINK = "<a href=\"%s\">%s</a>";
 
     @EJB
     private BatchExecutionBean batchExecutor;
@@ -23,32 +27,48 @@ public class BatchJobStartServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
 
+        long executionId;
 
-        if(req.getParameter("executionId") != null) {
-
+        if (req.getParameter("detailsForExecutionId") != null) {
             // have a look at what a job is up to
-            long executionId = Long.valueOf(req.getParameter("executionId"));
+            executionId = Long.valueOf(req.getParameter("detailsForExecutionId"));
             JobExecution execution = batchExecutor.getJobExecutionDetails(executionId);
             this.write("Execution Id \n " + execution, res);
 
-        }  else if (req.getParameter("executionIdToRestart") != null) {
-             // restart a job... ok
-            long executionIdToRestart = Long.valueOf(req.getParameter("executionIdToRestart"));
-            long newExecutionId = batchExecutor.restartJob(executionIdToRestart);
-            this.write(String.format("Batch execution %d is the result of a restart", newExecutionId), res);
+        } else if (req.getParameter("restartExecutionId") != null) {
+            // restart a job... ok
+            long executionIdToRestart = Long.valueOf(req.getParameter("restartExecutionId"));
+
+                executionId = batchExecutor.restartJob(executionIdToRestart);
+
+                this.write(String.format("Batch execution %d is the result of a restart", executionId), res);
+
         } else {
             // must be a new job you want
-            long executionId = batchExecutor.submitJob();
+            executionId = batchExecutor.submitJob();
             this.write(String.format("Batch execution %d is running", executionId), res);
         }
+
+
+        String contextPath = req.getContextPath();
+
+        this.write("<h2>Options</h2>", res);
+        this.writeLink(contextPath + SERVLET_MAPPING + "?detailsForExecutionId=" + executionId, "View details for Execution Id " + executionId, res);
+        this.writeLink(contextPath + SERVLET_MAPPING + "?restartExecutionId=" + executionId, "Restart Execution Id " + executionId, res);
+        this.writeLink(contextPath + SERVLET_MAPPING, "Submit a new job", res);
 
     }
 
     private void write(String message, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
-        PrintWriter out= res.getWriter();
-        out.write(message);
+        PrintWriter out = res.getWriter();
+        out.println(message);
+    }
 
 
+    private void writeLink(String url, String text, HttpServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
+        out.println(String.format(HYPERLINK, url, text));
+        out.println("<br/>");
     }
 }
